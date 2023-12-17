71e8002caec80a6bb2f222e7cdcfa29bb9841bda
januschung <januschung@gmail.com>
2023-12-17 12:14:36 -0800

    add default route test (#8)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import PrimarySearchAppBar from './components/SearchBar';
+import JobApplicationList from './components/JobApplicationList';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+jest.mock('./components/SearchBar')
+jest.mock('./components/JobApplicationList')
+
+it('Should render Job Application list on default route', async () => {
+  PrimarySearchAppBar.mockImplementation(() => <div>PrimarySearchAppBarMock</div>)
+  JobApplicationList.mockImplementation(() => <div>JobApplicationListrMock</div>)
+  render(<MemoryRouter>
+          <App />
+        </MemoryRouter>);
+  expect(screen.getByText("PrimarySearchAppBarMock")).toBeInTheDocument();
+  expect(screen.getByText("JobApplicationListrMock")).toBeInTheDocument();
 });
