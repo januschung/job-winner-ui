7ac3fe8b2dc2adcb431eaea616c66a16190a9268
Oseas Moran <oseasmoran73@gmail.com>
2024-10-12 11:10:56 -0700

    Fix: Form submission for profile

--- src/components/ProfileDialog.jsx
+++ src/components/ProfileDialog.jsx
@@ -74,180 +74,172 @@ export default function ProfileDialog({ profile, handleClose, open, setOpen }) {
   return (
     <Dialog open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description" fullWidth maxWidth="sm">
       <DialogTitle>Profile</DialogTitle>
-      <DialogContent dividers>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="firstName"
-              name="firstName"
-              label="First Name"
-              fullWidth
-              variant="standard"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={firstName} />
-          </Grid>
+      <form onSubmit={() => handleUpdateProfile(profile.id)}>
+        <DialogContent dividers>
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={11}>
+              <TextField
+                id="firstName"
+                name="firstName"
+                label="First Name"
+                fullWidth
+                variant="standard"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={firstName} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="lastName"
-              name="lastName"
-              label="Last Name"
-              fullWidth
-              variant="standard"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={lastName} />
-          </Grid>
+            <Grid item xs={11}>
+              <TextField
+                id="lastName"
+                name="lastName"
+                label="Last Name"
+                fullWidth
+                variant="standard"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={lastName} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="addressStreet1"
-              name="addressStreet1"
-              label="Address Street 1"
-              fullWidth
-              variant="standard"
-              value={addressStreet1}
-              onChange={(e) => setAddressStreet1(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={addressStreet1} />
-          </Grid>
+            <Grid item xs={11}>
+              <TextField
+                id="addressStreet1"
+                name="addressStreet1"
+                label="Address Street 1"
+                fullWidth
+                variant="standard"
+                value={addressStreet1}
+                onChange={(e) => setAddressStreet1(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={addressStreet1} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="addressStreet2"
-              name="addressStreet2"
-              label="Address Street 2"
-              fullWidth
-              variant="standard"
-              value={addressStreet2}
-              onChange={(e) => setAddressStreet2(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={addressStreet2} />
-          </Grid>
+            <Grid item xs={11}>
+              <TextField
+                id="addressStreet2"
+                name="addressStreet2"
+                label="Address Street 2"
+                fullWidth
+                variant="standard"
+                value={addressStreet2}
+                onChange={(e) => setAddressStreet2(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={addressStreet2} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="addressCity"
-              name="addressCity"
-              label="City"
-              fullWidth
-              variant="standard"
-              value={addressCity}
-              onChange={(e) => setAddressCity(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={addressCity} />
-          </Grid>
+            <Grid item xs={11}>
+              <TextField
+                id="addressCity"
+                name="addressCity"
+                label="City"
+                fullWidth
+                variant="standard"
+                value={addressCity}
+                onChange={(e) => setAddressCity(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={addressCity} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="addressState"
-              name="addressState"
-              label="State"
-              fullWidth
-              variant="standard"
-              value={addressState}
-              onChange={(e) => setAddressState(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={addressState} />
-          </Grid>
+            <Grid item xs={11}>
+              <TextField
+                id="addressState"
+                name="addressState"
+                label="State"
+                fullWidth
+                variant="standard"
+                value={addressState}
+                onChange={(e) => setAddressState(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={addressState} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="addressZip"
-              name="addressZip"
-              label="Zip Code"
-              fullWidth
-              variant="standard"
-              value={addressZip}
-              onChange={(e) => setAddressZip(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={addressZip} />
-          </Grid>
+            <Grid item xs={11}>
+              <TextField
+                id="addressZip"
+                name="addressZip"
+                label="Zip Code"
+                fullWidth
+                variant="standard"
+                value={addressZip}
+                onChange={(e) => setAddressZip(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={addressZip} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="linkedin"
-              name="linkedin"
-              label="LinkedIn"
-              fullWidth
-              variant="standard"
-              value={linkedin}
-              onChange={(e) => setLinkedin(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={linkedin} />
-          </Grid>
+            <Grid item xs={11}>
+              <TextField
+                id="linkedin"
+                name="linkedin"
+                label="LinkedIn"
+                fullWidth
+                variant="standard"
+                value={linkedin}
+                onChange={(e) => setLinkedin(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={linkedin} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="github"
-              name="github"
-              label="Github"
-              fullWidth
-              variant="standard"
-              value={github}
-              onChange={(e) => setGithub(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={github} />
-          </Grid>
+            <Grid item xs={11}>
+              <TextField
+                id="github"
+                name="github"
+                label="Github"
+                fullWidth
+                variant="standard"
+                value={github}
+                onChange={(e) => setGithub(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={github} />
+            </Grid>
 
-          <Grid item xs={11}>
-            <TextField
-              required
-              id="personalWebsite"
-              name="personalWebsite"
-              label="Personal Website"
-              fullWidth
-              variant="standard"
-              value={personalWebsite}
-              onChange={(e) => setPersonalWebsite(e.target.value)}
-            />
+            <Grid item xs={11}>
+              <TextField
+                id="personalWebsite"
+                name="personalWebsite"
+                label="Personal Website"
+                fullWidth
+                variant="standard"
+                value={personalWebsite}
+                onChange={(e) => setPersonalWebsite(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={1}>
+              <CopyButton text={personalWebsite} />
+            </Grid>
           </Grid>
-          <Grid item xs={1}>
-            <CopyButton text={personalWebsite} />
-          </Grid>
-        </Grid>
-        <Typography variant="overline" align="right" paragraph>
-          * Click the copy icon to copy to clipboard
-        </Typography>
-      </DialogContent>
-      <DialogActions>
-        <Button color="info" variant="outlined" startIcon={<CancelIcon />} onClick={handleClose}>
-          Cancel
-        </Button>
-        <Button color="info" variant="contained" startIcon={<SaveIcon />} onClick={() => handleUpdateProfile(profile.id)}>
-          Save
-        </Button>
-      </DialogActions>
+          <Typography variant="overline" align="right" paragraph>
+            * Click the copy icon to copy to clipboard
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button color="info" variant="outlined" startIcon={<CancelIcon />} onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button color="info" variant="contained" startIcon={<SaveIcon />} type="submit">
+            Save
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
