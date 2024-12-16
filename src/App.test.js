7d7c6213b19e8f122c1af299ecc9fea4de930ba3
Janus Chung <januschung@gmail.com>
2024-12-15 22:13:39 -0800

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import PrimarySearchAppBar from './components/PrimarySearchAppBar';
+import AppHeader from './components/AppHeader';
 import JobApplicationList from './components/JobApplicationList';
 
 
-jest.mock('./components/PrimarySearchAppBar')
+jest.mock('./components/AppHeader')
 jest.mock('./components/JobApplicationList')
 
 it('Should render Job Application list on default route', async () => {
-  PrimarySearchAppBar.mockImplementation(() => <div>PrimarySearchAppBarMock</div>)
+  AppHeader.mockImplementation(() => <div>AppHeaderMock</div>)
   JobApplicationList.mockImplementation(() => <div>JobApplicationListrMock</div>)
   render(<MemoryRouter>
           <App />
         </MemoryRouter>);
-  expect(screen.getByText("PrimarySearchAppBarMock")).toBeInTheDocument();
+  expect(screen.getByText("AppHeaderMock")).toBeInTheDocument();
 });
