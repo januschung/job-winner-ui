f504e21a0799a135151ca36a8e6d1fc2208c1562
Janus Chung <januschung@gmail.com>
2025-02-17 18:55:55 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useState, useMemo } from "react";
+import React from "react";
 import { CssBaseline } from "@mui/material";
-// import { createAppTheme } from "./config/theme";
 import AppHeader from "./components/AppHeader";
 import { SnackbarProvider } from './components/common/SnackbarContext';
 import { ThemeContextProvider } from "./components/ThemeContext";
 
 function App() {
-  // const [mode, setMode] = useState("light");
-
-  // const theme = useMemo(() => createAppTheme(mode), [mode]);
-
-  // const toggleColorMode = () => {
-  //   setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-  // };
-
+  
   return (
     <ThemeContextProvider>
       <CssBaseline />
