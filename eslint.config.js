7ea227127c5bfb4aaadc162174e9f2a91de4279b
Andrew Kramer <andrew.d.kramer.80@gmail.com>
2025-02-18 10:22:45 -0500

    Add ESLint with React plugin (#27) (#84)
    * Add ESLint with React plugin
    
    Signed-off-by: Andrew Kramer <andrew.d.kramer.80@gmail.com>
    
    * Add JSX runtime
    
    ---------
    
    Signed-off-by: Andrew Kramer <andrew.d.kramer.80@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+// eslint.config.js
+'use strict';
+
+const pluginJs = require("@eslint/js");
+const reactPlugin = require('eslint-plugin-react');
+const globals = require('globals');
+
+module.exports = [
+  pluginJs.configs.recommended,
+  reactPlugin.configs.flat.recommended, // This is not a plugin object, but a shareable config object
+  reactPlugin.configs.flat['jsx-runtime'], // Add this if you are using React 17+
+  {
+    plugins: {
+      js: pluginJs,
+      react: reactPlugin,
+    }
+  },
+  {
+    files: ["src/**/*.js"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
+  },
+];
