211b1f5123d7f4122fb991f20993ddce19e88466
januschung <januschung@gmail.com>
2025-02-23 11:57:47 -0800

    add status bar (#93)

---
+++ src/config/statusColor.js
@@ -0,0 +1,16 @@
+export const STATUS_COLORS = {
+  all: '#757575',
+  all_hover: '#9E9E9E',
+
+  open: '#66BB6A',
+  open_hover: '#81C784',
+
+  active: '#42A5F5',
+  active_hover: '#64B5F6',
+
+  rejected: '#EF5350',
+  rejected_hover: '#E57373',
+
+  ghosted: '#A67C52',
+  ghosted_hover: '#BF9060',
+};
